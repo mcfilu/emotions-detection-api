ef6532a2c32e3267cb21e44bc2c0da76cde0185b
mcfilu <filip.1olszewski@gmail.com>
2024-03-05 01:02:56 +0000

    changing the docstrin in filters.py

--- app/routers/filters.py
+++ app/routers/filters.py
@@ -12,6 +12,19 @@ async def image_upload(
         file: UploadFile = File(..., description="Uploaded File"),
         depth: int = Form(30, title="Depth", description="Blur Depth", ge=0, le=100)
 ):
+    """
+    Apply a blur effect to an uploaded image.
+
+    Parameters:
+    - file (UploadFile): The uploaded image file.
+    - depth (int): The depth of the blur effect (default: 30, range: 0-100).
+
+    Returns:
+    StreamingResponse: A streaming response containing the processed image with the blur effect applied.
+
+    Raises:
+    HTTPException: If the content type of the uploaded file is not supported (only 'image/jpeg' is supported).
+    """
     content_type = file.content_type
     if content_type not in ["image/jpeg"]:
         raise HTTPException(status_code=422, detail={"error": str("Invalid content type")})
